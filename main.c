21f05507002f5a7d2d57181d7ca3779b7ef4dd9c
palaioz <antonio.gosti@outlook.com>
2026-09-20 13:46:44 -0300

    add: file main.c

---
+++ main.c
@@ -0,0 +1,45 @@
+/*
+ * No nosso dia-a-dia, durante a resolução de problemas e sua
+ * implementação, nos deparamos com diferentes tipos de dados
+ * e manipulações desses dados, que precisam ser elaboradas.
+ * Com o uso de ferramentas de edição e compilação de programas,
+ * faça um programa para gerenciar o Controle de Medicamentos,
+ * conforme a descrição:
+ *      Uma farmácia pública controla os medicamentos disponibilizados
+ *      pelo governo às pessoas cadastradas. Desenvolva parte do
+ *      controle desta farmácia, fazendo o Controle de Medicamentos,
+ *      cujos dados são:
+ *          - Medicamentos;
+ *          - Princípio ativo;
+ *          - Laboratório;
+ *          - Quantidade em estoque;
+ *          - Unidade (caixa, ml, comprimido, ...);
+ *          - Data de validade;
+ *          - Preço custo (R$);
+ * Desenvolva um algoritmo com a estrutura de dados para
+ * armazenar os dados de N medicamentos. Posteriormente,
+ * execute cada uma das ações a seguir (apresente um Menu
+ * de opções ao usuário para facilitar a escolha):
+ *      - Listar todos os medicamentos e seus dados;
+ *      - Listar os medicamentos de um laboratório informado
+ *          pelo usuário;
+ *      - Calcular e mostrar a quantidade de medicamentos com
+ *          quantidade menor à informada pelo usuário;
+ *      - Cacular o valor total pago pelos medicamentos.
+ */
+
+#include <locale.h>
+#include <stdio.h>
+#include <string.h>
+// #include <windows.h>
+
+// --- DEFINIÇÃO DE FUNÇÕES ---
+
+void show_menu();
+
+// --- CÓDIGO PRINCIPAL ---
+
+int main() {
+  printf("Hello, World!");
+  return 0;
+}
